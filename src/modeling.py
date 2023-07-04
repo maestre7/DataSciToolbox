74f4ed6084413023c9bcdef6fe9724a957205627
JuditRoca <roca.tovar@gmail.com>
2023-07-04 21:51:16 +0200

    fun+test comparador modelos

--- src/modeling.py
+++ src/modeling.py
@@ -39,3 +39,58 @@ def export_import_model(model, path_model, name, save=True, open=False):
 
     return model_pretrained
 
+from sklearn.metrics import r2_score, mean_absolute_error, mean_absolute_percentage_error, mean_squared_error
+import warnings
+from sklearn.exceptions import UndefinedMetricWarning
+import numpy as np
+
+def comparar_modelos(modelos, X_test, y_test):
+    """
+    Funcion para comparar las metricas de varios modelos y devolver el modelo con mejores metricas.
+
+    Parametros
+    ----------
+        modelos: lista con los modelos a comparar.
+
+        X_test: variables a predecir.
+
+        y_test: predicción.
+        
+    Returns
+    -------
+        Mejor modelo: el modelo con mejores métricas.
+    """
+    mejor_modelo = None
+    mejor_r2 = -np.inf
+    mejor_mae = np.inf
+    mejor_mape = np.inf
+    mejor_mse = np.inf
+    
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", category=UndefinedMetricWarning)
+        
+        for modelo in modelos:
+            try:
+                y_pred = modelo.predict(X_test)
+
+                r2 = r2_score(y_test, y_pred)
+                mae = mean_absolute_error(y_test, y_pred)
+                mape = mean_absolute_percentage_error(y_test, y_pred)
+                mse = mean_squared_error(y_test, y_pred)
+
+                if r2 > mejor_r2:
+                    mejor_modelo = modelo
+                    mejor_r2 = r2
+                    mejor_mae = mae
+                    mejor_mape = mape
+                    mejor_mse = mse
+            except Exception as e:
+                print(f"Error al evaluar el modelo {modelo}: {str(e)}")
+    
+    print("Mejor modelo:", str(mejor_modelo))
+    print("Mejor R2 Score:", mejor_r2)
+    print("Mejor MAE:", mejor_mae)
+    print("Mejor MAPE:", mejor_mape)
+    print("Mejor MSE:", mejor_mse)
+    
+    return mejor_modelo
