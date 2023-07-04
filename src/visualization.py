190431b8c0629e84687392f34817f735a84bd533
Vihelmet <129219070+Vihelmet@users.noreply.github.com>
2023-07-05 00:13:43 +0200

    Update visualization.py

--- src/visualization.py
+++ src/visualization.py
@@ -1,11 +1,3 @@
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-import matplotlib.pyplot as plt
-import pandas as pd
-
 def plot_quality_counts(dataframe, column, color):
     """Crea un gráfico de barras que muestra el recuento de valores en una columna específica.
     
