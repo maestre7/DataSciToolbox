a457a9ddbabede5055beac3d2433ace62f2243aa
Giacomo <giamosalerno@gmail.com>
2023-07-05 01:08:58 +0200

    Resolución de conflictos

--- src/modeling.py
+++ src/modeling.py
@@ -1,11 +1,61 @@
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-import numpy, pandas
-from typing import Any
 
-def evaluacion_reg(nom_modelo:str, modelo:Any, X_train:numpy.ndarray, y_train:numpy.ndarray, X_test:numpy.ndarray, y_test:numpy.ndarray) -> pandas.DataFrame:  
-    '''
-    Función para evaluar las predicciones de un modelo de regresión de machine learning, devolviendo diferentes métricas en un dataframe.
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def evaluacion_clas(nom_modelo: str, modelo: Any, X_train: numpy.ndarray, y_train: numpy.ndarray, X_test: numpy.ndarray, y_test: numpy.ndarray, redondeo: int = None) -> pandas.DataFrame: # type: ignore
+    """
+    Función para evaluar las predicciones de un modelo de clasificación de machine learning, devolviendo diferentes métricas en un dataframe.
+    En caso de tratarse de una clasificación multiclase, el average de las métricas será None.
     Args:
         nom_modelo (str): El nombre del modelo.
         modelo (Any): Modelo de machine learning para hacer las predicciones.
@@ -13,27 +63,40 @@ def evaluacion_reg(nom_modelo:str, modelo:Any, X_train:numpy.ndarray, y_train:nu
         y_train (numpy.ndarray): Target de entrenamiento.
         X_test (numpy.ndarray): Variables predictivas de evaluación.
         y_test (numpy.ndarray): Target de evaluación.
+        redondeo (int): Cantidad de decimales para redondear las métricas. (default: None)
 
     Returns:
         pandas.DataFrame: Dataframe con todas las métricas de evaluación del modelo.
-    '''
+    """
     try:
         modelo.fit(X_train, y_train)
+        y_pred = modelo.predict(X_test)
+        y_pred_prob = modelo.predict_proba(X_test)
         
-        y_pred= modelo.predict(X_test)
+        if len(numpy.unique(y_test)) > 2:
+            average = None
+            multi_class = 'ovr'
+        else:
+            average = 'binary'
+            multi_class = 'raise'
         
-        mae = mean_absolute_error(y_test, y_pred)
-        mse = mean_squared_error(y_test, y_pred)
-        rmse = numpy.sqrt(mean_squared_error(y_test, y_pred))
-        r2 = r2_score(y_test, y_pred)
-        y_test_mean= y_test.mean()
-        mae_ratio= mae/y_test_mean
-        rmse_ratio= rmse/y_test_mean
+        accuracy = accuracy_score(y_test, y_pred)
+        precision = precision_score(y_test, y_pred, average=average)
+        recall = recall_score(y_test, y_pred, average=average)
+        f1 = f1_score(y_test, y_pred, average=average)
+        roc_auc = roc_auc_score(y_test, y_pred_prob, multi_class=multi_class)
         
-        result_df = pandas.DataFrame(data=[[nom_modelo, mae, mse, rmse, r2, mae_ratio, rmse_ratio]], 
-                                columns=["Model", 'MAE', 'MSE', 'RMSE', 'R2 Score', "MAE Ratio", "RMSE Ratio"])
+        if redondeo is not None:
+            accuracy = numpy.round(accuracy, redondeo)
+            precision = numpy.round(precision, redondeo)
+            recall = numpy.round(recall, redondeo)
+            f1 = numpy.round(f1, redondeo)
+            roc_auc = numpy.round(roc_auc, redondeo)
+        
+        result_df = pandas.DataFrame(data=[[nom_modelo, accuracy, precision, recall, f1, roc_auc]], 
+                                columns=["Model", 'Accuracy', 'Precision', 'Recall', 'F1 Score', 'ROC AUC'])
         return result_df
     
     except Exception as e:
         print("Error al evaluar el modelo'{}':".format(nom_modelo))
-        return None # type: ignore
+        return None # type: ignore  
