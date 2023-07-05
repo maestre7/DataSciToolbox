9d8cafe143c4b4ad559c65b136447ecc69578a74
JuditRoca <roca.tovar@gmail.com>
2023-07-05 15:56:35 +0200

    branch fun comparacion modelos

--- src/modeling.py
+++ src/modeling.py
@@ -1,54 +1,55 @@
-from typing import Any
-import numpy, pandas
+from sklearn.metrics import r2_score, mean_absolute_error, mean_absolute_percentage_error, mean_squared_error
+import warnings
+from sklearn.exceptions import UndefinedMetricWarning
+import numpy as np
 
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
-
-def evaluacion_clas(nom_modelo: str, modelo: Any, X_train: numpy.ndarray, y_train: numpy.ndarray, X_test: numpy.ndarray, y_test: numpy.ndarray, redondeo: int = None) -> pandas.DataFrame: # type: ignore
+def comparar_modelos(modelos, X_test, y_test):
     """
-    Función para evaluar las predicciones de un modelo de clasificación de machine learning, devolviendo diferentes métricas en un dataframe.
-    En caso de tratarse de una clasificación multiclase, el average de las métricas será None.
-    Args:
-        nom_modelo (str): El nombre del modelo.
-        modelo (Any): Modelo de machine learning para hacer las predicciones.
-        X_train (numpy.ndarray): Variables predictivas de entrenamiento.
-        y_train (numpy.ndarray): Target de entrenamiento.
-        X_test (numpy.ndarray): Variables predictivas de evaluación.
-        y_test (numpy.ndarray): Target de evaluación.
-        redondeo (int): Cantidad de decimales para redondear las métricas. (default: None)
+    Funcion para comparar las metricas de varios modelos y devolver el modelo con mejores metricas.
 
-    Returns:
-        pandas.DataFrame: Dataframe con todas las métricas de evaluación del modelo.
+    Parametros
+    ----------
+        modelos: lista con los modelos a comparar.
+
+        X_test: variables a predecir.
+
+        y_test: predicción.
+        
+    Returns
+    -------
+        Mejor modelo: el modelo con mejores métricas.
     """
-    try:
-        modelo.fit(X_train, y_train)
-        y_pred = modelo.predict(X_test)
-        y_pred_prob = modelo.predict_proba(X_test)
-        
-        if len(numpy.unique(y_test)) > 2:
-            average = None
-            multi_class = 'ovr'
-        else:
-            average = 'binary'
-            multi_class = 'raise'
-        
-        accuracy = accuracy_score(y_test, y_pred)
-        precision = precision_score(y_test, y_pred, average=average)
-        recall = recall_score(y_test, y_pred, average=average)
-        f1 = f1_score(y_test, y_pred, average=average)
-        roc_auc = roc_auc_score(y_test, y_pred_prob, multi_class=multi_class)
-        
-        if redondeo is not None:
-            accuracy = numpy.round(accuracy, redondeo)
-            precision = numpy.round(precision, redondeo)
-            recall = numpy.round(recall, redondeo)
-            f1 = numpy.round(f1, redondeo)
-            roc_auc = numpy.round(roc_auc, redondeo)
-        
-        result_df = pandas.DataFrame(data=[[nom_modelo, accuracy, precision, recall, f1, roc_auc]], 
-                                columns=["Model", 'Accuracy', 'Precision', 'Recall', 'F1 Score', 'ROC AUC'])
-        return result_df
+    mejor_modelo = None
+    mejor_r2 = -np.inf  #marcamos peor metrica posible
+    mejor_mae = np.inf
+    mejor_mape = np.inf
+    mejor_mse = np.inf
     
-    except Exception as e:
-        print("Error al evaluar el modelo'{}':".format(nom_modelo))
-        return None # type: ignore
+
+        
+    for modelo in modelos:
+        try:
+            y_pred = modelo.predict(X_test)
+
+            r2 = r2_score(y_test, y_pred)
+            mae = mean_absolute_error(y_test, y_pred)
+            mape = mean_absolute_percentage_error(y_test, y_pred)
+            mse = mean_squared_error(y_test, y_pred)
+
+            if r2 > mejor_r2:
+                mejor_modelo = modelo
+                mejor_r2 = r2
+                mejor_mae = mae
+                mejor_mape = mape
+                mejor_mse = mse
+        except Exception as e:
+            print(f"Error al evaluar el modelo {modelo}: {str(e)}")
+    
+        print("Mejor modelo:", str(mejor_modelo))
+        print("Mejor R2 Score:", mejor_r2)
+        print("Mejor MAE:", mejor_mae)
+        print("Mejor MAPE:", mejor_mape)
+        print("Mejor MSE:", mejor_mse)
+        
+        return mejor_modelo
     
