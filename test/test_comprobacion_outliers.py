f2516b3c8402c6767052d520558800b74e72a125
jaferdy <129390093+jaferdy@users.noreply.github.com>
2023-07-05 17:44:27 +0200

    Update test_comprobacion_outliers.py

--- test/test_comprobacion_outliers.py
+++ test/test_comprobacion_outliers.py
@@ -1,19 +1,46 @@
-import numpy as np
 import pandas as pd
-import seaborn as sns
-from src.preprocessing import comprobacion_outliers
+import pytest
 
-def test_comprobacion_outliers():
-    # Crear un DataFrame de ejemplo
-    data = {'A': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-            'B': [2, 4, 6, 8, 10, 12, 14, 16, 18, 20],
-            'C': [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]}
-    df = pd.DataFrame(data)
+# Importa la función que deseas probar
+from comprobacion_outliers import comprobacion_outliers
+
+# Fixture para generar un DataFrame de prueba
+@pytest.fixture
+def dataframe_prueba():
+    data = {
+        'columna1': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        'columna2': [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    }
+    return pd.DataFrame(data)
+
+# Prueba para verificar el cálculo de outliers
+def test_comprobacion_outliers(dataframe_prueba):
+    # Llama a la función con la columna 'columna1'
+    resultado = comprobacion_outliers(dataframe_prueba, 'columna1')
     
-    # Llamar a la función y obtener los resultados
-    result = comprobacion_outliers(df, 'A')
+    # Verifica que el resultado sea un diccionario con las claves correctas
+    assert isinstance(resultado, dict)
+    assert 'numero_outliers' in resultado
+    assert 'porcentaje_outliers' in resultado
     
-   
-    # Verificar el número de outliers y su porcentaje
-    assert len(result.outliers) == 0
-    assert result.outliers_percentage == 0.0
+    # Verifica que el número de outliers y el porcentaje sean correctos
+    assert resultado['numero_outliers'] == 0
+    assert resultado['porcentaje_outliers'] == 0.0
+
+    # Llama a la función con la columna 'columna2'
+    resultado = comprobacion_outliers(dataframe_prueba, 'columna2')
+
+    # Verifica que el número de outliers y el porcentaje sean correctos
+    assert resultado['numero_outliers'] == 0
+    assert resultado['porcentaje_outliers'] == 0.0
+
+# Prueba para verificar el manejo de errores
+def test_comprobacion_outliers_errores(dataframe_prueba):
+    # Llama a la función con una columna inexistente
+    with pytest.raises(KeyError):
+        comprobacion_outliers(dataframe_prueba, 'columna3')
+
+    # Llama a la función con un nombre de columna no válido (no string)
+    with pytest.raises(TypeError):
+        comprobacion_outliers(dataframe_prueba, 123)
+
