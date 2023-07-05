f3ad8cf20155c1ece907f3b6af5644662dd494b7
jaferdy <129390093+jaferdy@users.noreply.github.com>
2023-07-05 18:04:39 +0200

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -257,9 +257,9 @@ def split_and_encode_strings(column:pd.Series, use_encoding: bool = False ) -> p
         print("Ocurrió un error al separar y encodear las strings:", str(e))
         return None
 
-def eliminacion_outliers(dataframe:pd.DataFrame, nombre_columna:str):
+def eliminacion_outliers(dataframe: pd.DataFrame, nombre_columna: str):
     '''
-    Esta función elimina las filas del DataFrame que contienen valores atípicos (outliers) en una columna especifica.
+    Esta función elimina las filas del DataFrame que contienen valores atípicos (outliers) en una columna especificada.
 
     Args:
     - dataframe: DataFrame de Pandas que contiene los datos.
@@ -269,21 +269,16 @@ def eliminacion_outliers(dataframe:pd.DataFrame, nombre_columna:str):
     - Devuelve el DataFrame sin los valores atípicos de la columna especificada.
     '''
 
-    try:
-        if not isinstance(nombre_columna, str):
-            raise TypeError("El nombre de la columna debe ser un string.")
-        
-        df = dataframe.copy()
-        q1 = np.percentile(df[nombre_columna], 25)
-        q3 = np.percentile(df[nombre_columna], 75)
-        rango_intercuartilico = q3 - q1 
-        df = df[(df[nombre_columna] >= (q1 - 1.5 * rango_intercuartilico)) & (df[nombre_columna] <= (q3 + 1.5 * rango_intercuartilico))]
+    if not isinstance(nombre_columna, str):
+        raise TypeError("El nombre de la columna debe ser un string.")
 
-        return df
+    if nombre_columna not in dataframe.columns:
+        raise KeyError("La columna especificada no existe en el DataFrame.")
 
-    except KeyError:
-        print("Error: La columna especificada no existe en el DataFrame.")
-    except TypeError as e:
-        print("Error:", str(e))
-    except:
-        print("Error: Se produjo un problema al procesar la función. Por favor, revisa la documentación de la función, verifica que los parámetros de entrada estén correctamente indicados y revisa los datos de tu DataFrame.")
+    df = dataframe.copy()
+    q1 = np.percentile(df[nombre_columna], 25)
+    q3 = np.percentile(df[nombre_columna], 75)
+    rango_intercuartilico = q3 - q1
+    df = df[(df[nombre_columna] >= (q1 - 1.5 * rango_intercuartilico)) & (df[nombre_columna] <= (q3 + 1.5 * rango_intercuartilico))]
+
+    return df
