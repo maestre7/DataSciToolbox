94b2ea8028afcfffd163be70ec2bf075f627a2ca
jaferdy <129390093+jaferdy@users.noreply.github.com>
2023-07-05 17:30:22 +0200

    Update test_encoding_proporcional_target_binaria.py

--- test/test_encoding_proporcional_target_binaria.py
+++ test/test_encoding_proporcional_target_binaria.py
@@ -1,33 +1,35 @@
 import pandas as pd
 import pytest
-from src.preprocessing import encoding_proporcional_target_binaria
+from encoding_proporcional import encoding_proporcional_target_binaria
 
-@pytest.fixture
-def sample_dataframe():
-    data = {
-        'columna_categorica': ['A', 'B', 'A', 'B', 'C', 'C'],
-        'target': [1, 1, 0, 1, 0, 1]
-    }
-    return pd.DataFrame(data)
+# Crear una función de prueba utilizando pytest
+def test_encoding_proporcional_target_binaria():
+    # Crear un DataFrame de ejemplo
+    data = {'Columna_Categorica': ['A', 'B', 'A', 'B', 'C'],
+            'Target': [1, 0, 1, 1, 0]}
+    df = pd.DataFrame(data)
 
-def test_encoding_proporcional_target_binaria(sample_dataframe):
-    encoded_dataframe = encoding_proporcional_target_binaria(sample_dataframe, 'target', 'columna_categorica', 'encoded_column')
-    
-    # Comprobar si se creó la nueva columna
-    assert 'encoded_column' in encoded_dataframe.columns
-    
-    # Comprobar los valores encodeados
-    expected_values = [0.5, 0.6666666666666666, 0.5, 0.6666666666666666, 0.3333333333333333, 0.3333333333333333]
-    assert encoded_dataframe['encoded_column'].tolist() == expected_values
-    
-    # Comprobar que los valores encodeados son proporcionales a la variable target
-    assert encoded_dataframe['encoded_column'][0] == pytest.approx(0.5)
-    assert encoded_dataframe['encoded_column'][1] == pytest.approx(0.6666666666666666)
-    assert encoded_dataframe['encoded_column'][2] == pytest.approx(0.5)
-    assert encoded_dataframe['encoded_column'][3] == pytest.approx(0.6666666666666666)
-    assert encoded_dataframe['encoded_column'][4] == pytest.approx(0.3333333333333333)
-    assert encoded_dataframe['encoded_column'][5] == pytest.approx(0.3333333333333333)
-    
-    # Comprobar el manejo de errores
-    invalid_dataframe = pd.DataFrame({'columna_categorica': [1, 2, 3], 'target': [0, 1, 0]})
-    assert encoding_proporcional_target_binaria(invalid_dataframe, 'target', 'columna_categorica', 'encoded_column') is None
+    # Llamar a la función que se va a probar
+    encoded_df = encoding_proporcional_target_binaria(df, 'Target', 'Columna_Categorica', 'Nueva_Columna')
+
+    # Comprobar si la columna nueva fue creada correctamente
+    assert 'Nueva_Columna' in encoded_df.columns
+
+    # Comprobar si los valores de la columna nueva son correctos utilizando pytest.approx()
+    expected_values = [1.0, 0.5, 1.0, 0.5, 0.0]
+    assert encoded_df['Nueva_Columna'].tolist() == pytest.approx(expected_values)
+
+    # Probar un caso donde la columna target no existe
+    encoded_df = encoding_proporcional_target_binaria(df, 'Nonexistent', 'Columna_Categorica', 'Nueva_Columna')
+    assert encoded_df is None
+
+    # Probar un caso donde la columna columna_categorica no existe
+    encoded_df = encoding_proporcional_target_binaria(df, 'Target', 'Nonexistent', 'Nueva_Columna')
+    assert encoded_df is None
+
+    # Probar un caso donde la columna target no es binaria
+    data = {'Columna_Categorica': ['A', 'B', 'A', 'B', 'C'],
+            'Target': [1, 0, 1, 2, 0]}
+    df = pd.DataFrame(data)
+    encoded_df = encoding_proporcional_target_binaria(df, 'Target', 'Columna_Categorica', 'Nueva_Columna')
+    assert encoded_df is None
