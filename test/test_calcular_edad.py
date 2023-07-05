4b64cc4c74deb5b1052db8d22a65ddec0e42d71b
GabrielaRomina <gabrielarominalupas@gmail.com>
2023-07-05 10:46:29 +0200

    todas las revisiones

--- test/test_calcular_edad.py
+++ test/test_calcular_edad.py
@@ -4,11 +4,7 @@ from dateutil.relativedelta import relativedelta
 import os
 import sys
 
-# Agregar el directorio raíz del proyecto a la ruta de búsqueda de módulos
-root_dir = os.path.dirname(os.path.dirname(os.path.abspath('preprocessing.py')))
-sys.path.append(root_dir)
-
-from src.preprocessing import calcular_edad
+from preprocessing import calcular_edad
 
 @pytest.fixture
 def sample_data():
